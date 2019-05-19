c353fbe8968b8ad6f78d04419b68e1bf344b942e
Ruhan dos Reis <mobbyd1@gmail.com>
2019-05-19 20:15:33 -0300

    Add open api 3 example

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/videos', function(req, res, next) {
-  res.send([{ id: 'Express' }])
+  res.send([
+    { id: 'Express', description: null }
+  ])
 })
 
 export default router
